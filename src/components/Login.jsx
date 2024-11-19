c5178c3d7fd791ce40ecaa4fafdd657be0332bb0
Carlos Figueredo <carlosalberto.figueredoquevedo@gmail.com>
2024-11-19 00:09:09 -0300

    Correccion en el titulo de la pagina

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
     e.preventDefault();
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      navigate('/eventos');
+      navigate('/events');
     } catch (error) {
       console.error(error);
       if (error){
